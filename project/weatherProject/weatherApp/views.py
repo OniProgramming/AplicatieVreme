02566e51560a8380cd8b7e86fee0a063f31c53a4
OniProgramming <118599597+OniProgramming@users.noreply.github.com>
2023-02-01 03:51:50 -0800

    Update views.py

--- project/weatherProject/weatherApp/views.py
+++ project/weatherProject/weatherApp/views.py
@@ -9,6 +9,12 @@ def index (request):
     if request.method == 'POST':
         city = request.POST['city']
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=9e6f5da9f848e7b683f79045e338f769').read()
+        # Because we workwith APIs, openweathermap.org it generate a unique API the moment that you made an account. Therefore, if you want to copy this API
+        # from above it will not work for you being my personal generated API. What you need to do is to go on the website, make an account, and then
+        # after log in you will find your own API. Copy it and paste it where it need to be: 
+        # source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + 
+        # city + '&units=metric&appid=<Your API from https://openweathermap.org/api account>').read()
+        
         # convert the json into a dictionary
         list_of_data = json.loads(source) 
         # dictionary
